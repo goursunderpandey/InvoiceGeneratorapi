20fd9b3d6622b029b7b04f2802f53b1eaf9cd005
Goursunder Pandey <sunderpandey53@gmail.com>
2025-09-21 19:44:45 +0530

    change in route

--- src/Customer/Route/Customer.route.js
+++ src/Customer/Route/Customer.route.js
@@ -5,8 +5,8 @@ const jwtmiddleware = require("../../Middleware/authMiddleware")
 
 router.post("/addcustomer",jwtmiddleware, customerController.createCustomer);
 router.get("/getcustomer", jwtmiddleware,customerController.getCustomers);
-router.get("/getcustomer:id",jwtmiddleware, customerController.getCustomerById);
-router.put("/updatecustomer:id",jwtmiddleware, customerController.updateCustomer);
+router.get("/getcustomer/:id",jwtmiddleware, customerController.getCustomerById);
+router.put("/updatecustomer/:id",jwtmiddleware, customerController.updateCustomer);
 router.delete("/deletecustomer/:id",jwtmiddleware, customerController.deleteCustomer);
 
 module.exports = router;
