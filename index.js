6c8ff88740eafe593f9db5ddaabc9091f618ac24
Goursunder Pandey <sunderpandey53@gmail.com>
2025-09-28 21:53:53 +0530

    fix image bug

--- index.js
+++ index.js
@@ -2,32 +2,35 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path"); // Add this import
 const PORT = 8080;
-
 
 app.use(cors());
 let Database = require("./Database.js");
 const authRoutes = require("./src/Auth/Route/Auth.route.js");
 const Customer = require("./src/Customer/Route/Customer.route.js");
 const Item = require("./src/Item/Route/Item.routes.js");
-const Sales = require("./src/Sales/Routes/Sales.route.js")
-
-
+const Sales = require("./src/Sales/Routes/Sales.route.js");
 
 // Middleware
 app.use(express.json());
 
+// Serve static files from uploads directory
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
 // Mount routes
 app.use("/V1/auth", authRoutes);
-app.use("/V1",Customer);
+app.use("/V1", Customer);
 app.use("/V1", Item);
-app.use("/V1",Sales);
+app.use("/V1", Sales);
+
 
 
 Database.connect()
   .then(() => {
     app.listen(PORT, () => {
-      console.log(`server running on ${PORT}`);
+      console.log(`Server running on http://localhost:${PORT}`);
+
     });
   })
   .catch((err) => {
